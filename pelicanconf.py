695d063bcf7debffdf3ca210408f64f3db3df76c
Mohammed-Fahd-Mustafa-A <230071601120@crescent.education>
2025-06-27 17:55:10 +0530

    testing

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'Fahd'  # use your name
+SITENAME        = 'My Python Notes' # use title
+GITHUB_USERNAME = 'Mohammed-Fahd-Mustafa-A' # use your username
 
 
 # Blogroll
